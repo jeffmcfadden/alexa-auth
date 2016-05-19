d8f2836dfe18ca4285cee6158c9d05ba9623f53f
Jeff McFadden <jeff@forgeapps.com>
2016-05-19 16:19:46 -0700

    Add the two basic controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/auth' => 'auth#auth', as: :auth_get
+  post '/auth' => 'auth#auth', as: :auth_post
+
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
