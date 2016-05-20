a87bddfdc2b646fbe69c3dadf15f7ef183e09346
Jeff McFadden <jeff@forgeapps.com>
2016-05-20 11:06:29 -0700

    Let's try this.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,9 @@
 class AuthController < ApplicationController
 
   def auth
+    url = "#{ENV['AUTH_REDIRECT_URL']}?state=#{params[:state]}&code=#{ENV['AUTHORIZATION_TOKEN']}"
     
+    redirect_to url and return
   end
   
   def access_token
